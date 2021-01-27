56abf562c1e9e567ad1e970afc1a22a3b197b450
aebby <abraham@softcom.ng>
2021-01-27 21:04:12 +0100

    fix unhandled exceptions

--- controller/app.js
+++ controller/app.js
@@ -1,39 +1,5 @@
 const { request, response: _response } = require('express');
-const Joi = require('joi');
-
-function responseBuilder({
-  status,
-  error,
-  field,
-  field_value,
-  condition,
-  condition_value,
-}) {
-  return {
-    message: `field ${field} successfully validated.`,
-    status,
-    data: {
-      validation: {
-        error,
-        field,
-        field_value,
-        condition,
-        condition_value,
-      },
-    },
-  };
-}
-
-function findVowels(value) {
-  const vowels = ['a', 'e', 'i', 'o', 'u'];
-  if (value !== undefined && value.length > 0) {
-    const firstIndex = value[0];
-    if (vowels.includes(firstIndex)) {
-      return `an ${value}`;
-    }
-    return `a ${value}`;
-  }
-}
+const { errorBuilder, responseBuilder } = require('../helpers/response');
 
 /**
  *
@@ -59,135 +25,57 @@ function rootController(req, res) {
  * @param {request} req
  * @param {response} res
  */
-function validateField(req, res, next) {
-  const CONDITIONS = ['eq', 'neq', 'gt', 'gte', 'contains'];
-  const schema = Joi.object({
-    rule: Joi.object({
-      field: Joi.string().required(),
-      condition: Joi.string()
-        .valid(...CONDITIONS)
-        .required(),
-      condition_value: Joi.any().required(),
-    }).required(),
-    data: Joi.any().required(),
-  });
+function validateField(req, res) {
+  try {
+    const { rule, data } = req.body;
+    const { condition, condition_value, field } = rule;
+    let response;
+    let code = 400;
+    let field_value;
+    const fieldSplit = field.split('.');
 
-  const validation = schema.validate(req.body);
-  if (validation.error) {
-    const errType = validation.error.details[0].context.type;
-    console.log(errType);
-    return res.status(400).json({
-      message: validation.error.details[0].message
-        .replace(/['"]+/g, '')
-        .replace('must', 'should')
-        .replace('of type object', `${findVowels(errType)}`),
-      status: 'error',
-      data: null,
-    });
-  }
-
-  const { rule, data } = req.body;
-  const { condition, condition_value, field } = rule;
-
-  let response;
-  let code = 400;
-
-  for (let i = 0; i < Object.keys(data).length; i++) {
-    const keys = Object.keys(data);
-    if (typeof data[keys[i]] === 'object') {
-      if (!Object.keys(data[keys[i]]).includes(field.split('.')[1])) {
-        return res.status(400).json({
-          message: `field ${field} is missing from data.`,
-          status: 'error',
-          data: null,
-        });
+    if (fieldSplit.length > 1) {
+      for (let i = 0; i < Object.keys(data).length; i++) {
+        const keys = Object.keys(data);
+        if (typeof data[keys[i]] === 'object') {
+          if (!Object.keys(data[keys[i]]).includes(fieldSplit[1])) {
+            return res.status(400).json({
+              message: `field ${field} is missing from data.`,
+              status: 'error',
+              data: null,
+            });
+          }
+        }
       }
     }
-  }
 
-  if(!Object.keys(data).includes(field.split('.')[0])){
-    return res.status(400).json({
-      message: `field ${field} is missing from data.`,
-      status: 'error',
-      data: null,
+    if (!Object.keys(data).includes(fieldSplit[0])) {
+      return res.status(400).json({
+        message: `field ${field} is missing from data.`,
+        status: 'error',
+        data: null,
+      });
+    }
+
+    if (fieldSplit.length > 1) {
+      for (let i = 0; i < fieldSplit.length; i++) {
+        const m = data[fieldSplit[0]];
+        field_value = m[fieldSplit[i]];
+      }
+    } else {
+      field_value = data[field];
+    }
+
+    const error = errorBuilder({
+      field,
+      field_value,
+      condition,
+      condition_value,
     });
-  }
-  const fieldSplit = field.split('.');
-  let field_value;
-  for (let i = 0; i < fieldSplit.length; i++) {
-    const m = data[fieldSplit[0]];
-    field_value = m[fieldSplit[i]];
-  }
-  const error = {
-    message: `field ${field} failed validation.`,
-    status: 'error',
-    data: {
-      validation: {
-        error: true,
-        field,
-        field_value,
-        condition,
-        condition_value,
-      },
-    },
-  };
-  switch (condition) {
-    case 'eq':
-      if (condition_value === field_value) {
-        code = 200;
-        response = responseBuilder({
-          condition,
-          condition_value,
-          field,
-          field_value,
-          error: false,
-          status: 'success',
-        });
-      } else { response = error; code = 400 }
-      break;
-    case 'gte':
-      if (field_value >= condition_value) {
-        code = 200;
-        response = responseBuilder({
-          condition,
-          condition_value,
-          field,
-          field_value,
-          error: false,
-          status: 'success',
-        });
-      } else { response = error; code = 400 }
-      break;
-    case 'gt':
-      if (field_value > condition_value) {
-        code = 200;
-        response = responseBuilder({
-          condition,
-          condition_value,
-          field,
-          field_value,
-          error: false,
-          status: 'success',
-        });
-      } else { response = error; code = 400 }
-      break;
-    case 'neq':
-      if (field_value !== condition_value) {
-        code = 200;
-        response = responseBuilder({
-          condition,
-          condition_value,
-          field,
-          field_value,
-          error: false,
-          status: 'success',
-        });
-      } else { response = error; code = 400 }
-      break;
-    case 'contains':
-      if (field_value.length !== undefined && field_value.length >= 0) {
-        console.log(field_value.includes(condition_value));
-        if (field_value.includes(condition_value)) {
+    
+    switch (condition) {
+      case 'eq':
+        if (condition_value === field_value) {
           code = 200;
           response = responseBuilder({
             condition,
@@ -197,15 +85,95 @@ function validateField(req, res, next) {
             error: false,
             status: 'success',
           });
-        } else { response = error; code = 400 }
-      } else { response = error; code = 400 }
-      break;
-    default:
-      code = 400
-      response = error;
-    break;
+        } else {
+          response = error;
+          code = 400;
+        }
+        break;
+      case 'gte':
+        if (field_value >= condition_value) {
+          code = 200;
+          response = responseBuilder({
+            condition,
+            condition_value,
+            field,
+            field_value,
+            error: false,
+            status: 'success',
+          });
+        } else {
+          response = error;
+          code = 400;
+        }
+        break;
+      case 'gt':
+        if (field_value > condition_value) {
+          code = 200;
+          response = responseBuilder({
+            condition,
+            condition_value,
+            field,
+            field_value,
+            error: false,
+            status: 'success',
+          });
+        } else {
+          response = error;
+          code = 400;
+        }
+        break;
+      case 'neq':
+        if (field_value !== condition_value) {
+          code = 200;
+          response = responseBuilder({
+            condition,
+            condition_value,
+            field,
+            field_value,
+            error: false,
+            status: 'success',
+          });
+        } else {
+          response = error;
+          code = 400;
+        }
+        break;
+      case 'contains':
+        if (field_value.length !== undefined && field_value.length >= 0) {
+          console.log(field_value.includes(condition_value));
+          if (field_value.includes(condition_value)) {
+            code = 200;
+            response = responseBuilder({
+              condition,
+              condition_value,
+              field,
+              field_value,
+              error: false,
+              status: 'success',
+            });
+          } else {
+            response = error;
+            code = 400;
+          }
+        } else {
+          response = error;
+          code = 400;
+        }
+        break;
+      default:
+        code = 400;
+        response = error;
+        break;
+    }
+    return res.status(code).json(response);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      message: 'An unexpected error occured.',
+      status: 'error',
+      data: null,
+    });
   }
-  return res.status(code).json(response);
 }
 
 module.exports = {
--- routes/app.js
+++ routes/app.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const { rootController, validateField } = require('../controller/app');
+const { validateRuleSchema } = require('../middleware/validation');
 
 const router = express.Router();
 
 router.get('/', rootController);
-router.post('/validate-rule', validateField);
+router.post('/validate-rule', validateRuleSchema, validateField);
 
 module.exports = router;
